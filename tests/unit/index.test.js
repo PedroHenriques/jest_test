09140f50175fd7257b382585c4805a74908ec805
Pedro Henriques <pedro@pedrojhenriques.com>
2025-03-06 01:09:17 +0000

    a

---
+++ tests/unit/index.test.js
@@ -0,0 +1,37 @@
+'use strict';
+const sut = require('../../src/service1/index');
+
+const react = require('react');
+jest.mock('react', ()=>({
+  ...jest.requireActual('react'),
+  useState: jest.fn(),
+}))
+
+describe('Game', () => {
+  const doubles = {};
+
+  beforeEach(() => {
+    doubles.setHistory = jest.fn();
+
+    react.useState.mockReturnValue([[[]], doubles.setHistory]);
+  });
+
+  it('should call useState, from the react module, once', () => {
+    sut.default();
+    expect(react.useState.mock.calls.length).toBe(1);
+  });
+
+  describe('1st call to useState, from the react module', () => {
+    it('should have 1 argument', () => {
+      sut.default();
+      expect(react.useState.mock.calls[0].length).toBe(1);
+    });
+
+    describe('1st argument', () => {
+      it('should be an [][] filled with 9 null values', () => {
+        sut.default();
+        expect(react.useState.mock.calls[0][0]).toEqual([[null,null,null,null,null,null,null,null,null]]);
+      });
+    });
+  });
+});
